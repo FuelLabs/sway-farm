11d0aed5a09b0f9ef6c6a0ed2ccf305b5548a4dc
sarahschwartz <58856580+sarahschwartz@users.noreply.github.com>
2023-03-31 11:33:26 -0300

    styling changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,15 @@ import { useState, useEffect, useMemo } from "react";
 import { useIsConnected } from './hooks/useIsConnected';
 import { useFuel } from './hooks/useFuel';
 import { ContractAbi__factory } from "./contracts";
-import { Link } from "@fuel-ui/react";
+import { Link, Button } from "@fuel-ui/react";
+import { cssObj } from "@fuel-ui/css";
 import { WalletLocked } from "fuels";
 import Game from './components/Game';
 import './App.css';
 import { CONTRACT_ID } from "./constants";
+
+const myWallet = new WalletLocked("fuel13jajm0wphz2lkuv927p0rrv2v9akjaapx4umtp7yccnc2q05s2pszhs8aq");
+console.log("WALLET:", myWallet.address.toB256());
 
 function App() {
   const [wallet, setWallet] = useState<WalletLocked>();
@@ -40,11 +44,11 @@ function App() {
           {isConnected ? (
             <Game contract={contract}/>
           ) : (
-              <button onClick={() => fuel.connect()}>Connect Wallet</button>
+              <Button css={styles.button} onPress={() => fuel.connect()}>Connect Wallet</Button>
           )}
         </div>
       ) : (
-        <div>
+        <div style={{marginTop: "200px"}}>
           Download the{" "}
           <Link
             target="_blank"
@@ -61,3 +65,20 @@ function App() {
 }
 
 export default App;
+
+const styles = {
+  button: cssObj({
+      fontFamily: 'pressStart2P',
+      fontSize: '$sm',
+      mr: '-8px',
+      mt: '200px',
+      color: '#4c2802',
+      border: '2px solid #754a1e',
+      backgroundColor: '#754a1e',
+      '&:hover': {
+        color: '#ac7339',
+        backgroundColor: '#46677d !important',
+        boxShadow: 'none !important'
+      }
+  })
+}
