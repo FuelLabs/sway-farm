6170d860a3f073365b028113f1bc5a7949909ae8
sarahschwartz <58856580+sarahschwartz@users.noreply.github.com>
2024-02-02 12:59:58 -0700

    fix styling

--- frontend/src/components/show/Inventory.tsx
+++ frontend/src/components/show/Inventory.tsx
@@ -9,23 +9,23 @@ interface InventoryProps {
 export default function Inventory({ seeds, items }: InventoryProps) {
   return (
     <Box css={styles.container}>
-      <div style={styles.box}>
+      <Box css={styles.box}>
         {/* eslint-disable-next-line jsx-a11y/alt-text */}
         <Image css={styles.img} src={'images/tomato_seed_bag.png'} />
         <Box css={styles.numContainer}>
-          <div style={styles.num}>
+          <Box css={styles.num}>
             {seeds > 99 ? '99+' : seeds.toLocaleString()}
-          </div>
+          </Box>
         </Box>
-      </div>
+      </Box>
 
-      <div style={styles.box}>
+      <Box css={styles.box}>
         {/* eslint-disable-next-line jsx-a11y/alt-text */}
         <Image css={styles.img} src={'images/tomato.png'} />
         <Box css={styles.numContainer}>
-          <div style={styles.num}>{items > 99 ? '99+' : items}</div>
+          <Box css={styles.num}>{items > 99 ? '99+' : items}</Box>
         </Box>
-      </div>
+      </Box>
     </Box>
   );
 }
@@ -59,8 +59,8 @@ const styles = {
   },
   numContainer: cssObj({
     position: 'absolute',
-    bottom: '54px',
-    width: '64px',
+    bottom: '20px',
+    width: '60px',
     display: 'flex',
     justifyContent: 'flex-end',
     '@sm': {
@@ -68,8 +68,8 @@ const styles = {
       width: '80px',
     },
   }),
-  num: {
-    fontSize: '12px',
+  num: cssObj({
+    fontSize: "10px",
     fontFamily: 'pressStart2P',
     width: '25px',
     height: '25px',
@@ -80,13 +80,12 @@ const styles = {
     '@sm': {
       width: '35px',
       height: '35px',
-      fontSize: '16px',
     },
-  },
-  box: {
+  }),
+  box: cssObj({
     width: '60px',
     '@sm': {
       width: '80px',
     },
-  },
+  }),
 };
