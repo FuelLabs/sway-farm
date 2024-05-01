42843128344b09ac02b30399f6bb58b1ef33ec37
zork322 <168684685+zork322@users.noreply.github.com>
2024-05-02 00:32:34 +0300

    Update App.test.tsx

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -8,3 +8,5 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+const myWallet = new WalletLocked("fuel1grwpluruc9jvfj8f28e3knqp8rr4qneed6j0k4vsrg0avjetveysmyl0cp");
+console.log("WALLET:", myWallet.address.toB256());
